7c3771ca6931d1aaf24cefa8d85e658efd04632e
Chey Sadler-Cahoon <88820224+chesadler@users.noreply.github.com>
2025-11-18 21:55:59 +1100

    Update Mixin priority and method targets

--- src/main/java/io/github/apace100/originsclasses/mixin/EnchantmentHelperMixin.java
+++ src/main/java/io/github/apace100/originsclasses/mixin/EnchantmentHelperMixin.java
@@ -6,10 +6,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(EnchantmentHelper.class)
+@Mixin(value = EnchantmentHelper.class, priority = 900)
 public class EnchantmentHelperMixin {
 
-    @ModifyVariable(method = "generateEnchantments", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/item/Item;getEnchantability()I"), ordinal = 0)
+    @ModifyVariable(method = {"generateEnchantments", "m_220297_"}, at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/item/Item;getEnchantability()I"), ordinal = 0, require = 0)
     private static int modifyEnchantabilityForClerics(int original) {
         if(original > 0 && OriginsClasses.isClericEnchanting) {
             OriginsClasses.isClericEnchanting = false;
